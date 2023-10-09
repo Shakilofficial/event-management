dec81ac2ccc9d6a3d807376f489d35a6415a1c24
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-10-09 22:14:14 +0600

    aded questions files with answer

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,15 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCvzLZwo-VtDjZI8_ms-UYVIPt_9Q0p5L8",
-  authDomain: "crispberry-events.firebaseapp.com",
-  projectId: "crispberry-events",
-  storageBucket: "crispberry-events.appspot.com",
-  messagingSenderId: "956008644982",
-  appId: "1:956008644982:web:20bd7eaae21de086403a57"
+  apiKey:import.meta.env.VITE_APIKEY, 
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID, 
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET, 
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+export default app
